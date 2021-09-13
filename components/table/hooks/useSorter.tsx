37366ea840f8b3c02b93dd523020e9fcf42533b6
Katsiaryna Pustakhod <Katsiaryna_Pustakhod@epam.com>
2021-09-13 15:36:30 +0300

    Make table columns with sorting focusable and keyboard accessible.

--- components/table/hooks/useSorter.tsx
+++ components/table/hooks/useSorter.tsx
@@ -179,6 +179,7 @@ function injectSorter<RecordType>(
           const cell: React.HTMLAttributes<HTMLElement> =
             (column.onHeaderCell && column.onHeaderCell(col)) || {};
           const originOnClick = cell.onClick;
+          const keyboardOnClick = cell.onKeyPress;
           cell.onClick = (event: React.MouseEvent<HTMLElement>) => {
             triggerSorter({
               column,
@@ -191,8 +192,25 @@ function injectSorter<RecordType>(
               originOnClick(event);
             }
           };
+          cell.onKeyPress = (event: React.KeyboardEvent<HTMLElement>) => {
+            const { code } = event;
+            if (code === 'Enter') {
+              triggerSorter({
+                column,
+                key: columnKey,
+                sortOrder: nextSortOrder,
+                multiplePriority: getMultiplePriority(column),
+              });
+
+              if (keyboardOnClick) {
+                keyboardOnClick(event);
+              }
+            }
+          };
+
 
           cell.className = classNames(cell.className, `${prefixCls}-column-has-sorters`);
+          cell.tabIndex = 0;
 
           return cell;
         },
